77240de4cdefb7be000c74719ef27fc6f089c9d2
LiuJunjie0619 <jy024426122@sina.com>
2019-03-14 19:40:13 +0800

    项目初始化

--- database.py
+++ database.py
@@ -10,11 +10,13 @@ app.config.from_object(config.config_dict['config'])
 db = SQLAlchemy(app)
 
 
+# 父类
 class Base(object):
-    create_time = db.Column(db.DateTime, default=datetime.now())
-    update_time = db.Column(db.DateTime, default=datetime.now())
+    create_time = db.Column(db.DateTime, default=datetime.now())    # 数据创建时间
+    update_time = db.Column(db.DateTime, default=datetime.now())    # 数据修改时间
 
 
+# 管理员表
 class Admin(Base, db.Model):
     __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True)
@@ -22,39 +24,62 @@ class Admin(Base, db.Model):
     pass_hash = db.Column(db.String(200), nullable=False)
 
 
-class User(Base):
+# 用户表
+class User(Base, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    nick_name = db.Column(db.String(20), index=True)
+    nick_name = db.Column(db.String(20), index=True)                    # 昵称
     password_has = db.Column(db.String(200), nullable=False)
-    mobile = db.Column(db.String(11), nullable=False)
-    avatar_url = db.Column(db.String(256))
-    last_login = db.Column(db.DateTime)
-    signature = db.Column(db.String(512))
-    gender = db.Column(db.String(10), default='Man', nullable=False)
+    mobile = db.Column(db.String(11), nullable=False)                   # 手机号
+    avatar_url = db.Column(db.String(256))                              # 用户头像路径
+    last_login = db.Column(db.DateTime, default=datetime.now())         # 最后登录时间
+    signature = db.Column(db.String(512))                               # 用户签名
+    gender = db.Column(db.String(10), default='Man', nullable=False)    # 性别
+    news = db.relationship('News', backref='author', lazy='dynamic')    # 用户发布的新闻
+    news_collection = db.relationship('News', secondary='table_user_news', lazy='dynamic')  # 用户收藏的新闻
 
 
-class Category(Base):
+# 用户收藏，多对多 关系表
+table_user_news = db.Table('user_collection',
+                           db.Column('id', db.Integer, primary_key=True),
+                           db.Column('user_id', db.Integer, db.ForeignKey('user.id')),  # 用户Id
+                           db.Column('news_id', db.Integer, db.ForeignKey('news.id')))  # 新闻Id
+
+
+# 新闻分类表
+class Category(Base, db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
+    relate_news = db.relationship('News', backref='relate_category', lazy='dynamic')    # 分类下的新闻列表
 
 
-class News(Base):
+# 新闻表
+class News(Base, db.Model):
     __tablename__ = 'news'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30))
-    source = db.Column(db.String(30))
-    index_image_u = db.Column(db.String(100))
-    digest = db.Column(db.String(255))
-    clicks = db.Column(db.Integer)
-    content = db.Column(db.Text)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    status = db.Column(db.Integer)
-    reason = db.Column(db.String(100))
+    title = db.Column(db.String(30))            # 新闻标题
+    source = db.Column(db.String(30))           # 新闻来源
+    index_image_u = db.Column(db.String(100))   # 新闻展示的缩略图路径
+    digest = db.Column(db.String(255))          # 新闻摘要
+    clicks = db.Column(db.Integer)              # 新闻被点击次数
+    content = db.Column(db.Text)                # 新闻内容
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))   # 新闻分类Id
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))           # 新闻发布人Id
+    status = db.Column(db.Integer)              # 新闻状态 1审核中 2通过 3未通过
+    reason = db.Column(db.String(100))          # 审核失败的原因
+
+
+# 新闻评论表
+class Comment(Base, db.Model):
+    __tablename__='comment'
+    id = db.Column(db.Integer, primary_key=True)
+    news_id = db.Column(db.Integer, db.ForeignKey("news.id"))       # 新闻Id
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))       # 用户Id,即：谁发布的评论
+    content = db.Column(db.String(255))                             # 新闻内容
 
 
 if __name__ == '__main__':
+    db.drop_all()
     db.create_all()
     app.run(port=7865)
